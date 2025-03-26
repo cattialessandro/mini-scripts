6436441162b6512b9e2ebd6cafbe2781d0debb3a
cattialessandro <168655570+cattialessandro@users.noreply.github.com>
2025-03-26 20:37:27 +0100

    correzione e miglioramenti del codice
    script per sommare una lista di numeri interi attraverso una funzione "somma" scritta manualmente.

--- somma.py
+++ somma.py
@@ -1,22 +1,54 @@
-# funzione
+#!/usr/bin/python3
+
+# QUESTO CODICE È STATO CREATO DA ALESSANDRO CATTI
+# IL CODICE È STATO PUBBLICATO SU GITHUB A ANCHE SCOPO DIDATTICO
+# È POSSIBILE RIUTILLIZZARE IL CODICE :-)
+
+# BREVE DESCRIZIONE DELLO SCRIPT:
+# script per sommare una lista di numeri interi attraverso una funzione "somma" scritta manualmente
+
+
+# DICHIARAZIONE DELLA FUNZIONE:
 def somma(nums, s=0):
+    # ciclo che ad ogni iterazione somma ogni numero contenuto nella lista "nums"
     for num in nums:
-        s += num
+        s += num # la somma viene aggiornata aggiungendo "num" a "s"
     return s
 
-# esempio di utilizzo
+# ESEMPIO DI UTILIZZO:
+
+# creo una lista che dovrà contenere i numeri da sommare
 nums = list()
+
+# variabile "flag" per il ciclo while
 inserimento = True
+
+# inizializzo la variabile che contiene un singolo numero
+num = 0
+
+# ciclo che serve a popolare la lista dei numeri "nums"
 while inserimento:
+    
+    # uso un "try except" per gestire eventuali errori di digitazione e di input
     try:
-        num = int(input("Inserisci un numero [0 o VUOTO per terminare]: "))
+        # l'input inserito dovrà essere un numero intero
+        num = int(input("Inserisci un numero [0 per terminare]: "))
+        
+        # se il numero inserito corrisponde a 0, viene interrotto il ciclo
+        if num == 0:
+            inserimento = False
+            
+        # la lista viene popolata usando la funzione di "append"
+        nums.append(num)
+        
+    # gestione delle eccezioni, tipo errori di digitazione o tipo di dato diverso da un numero
     except:
+        # stampo a schermo un messaggio di errore e resetto "num" per evitare problemi
+        print("ERRORE DI DIGITAZIONE, RIPROVA!")
         num = 0
-    if num == 0:
-        inserimento = False
-    nums.append(num)
-
+        
+# la funzione è pronta per essere utilizzata
 s = somma(nums)
-
+# RISULTATO FINALE
 print("SOMMA NUMERI:", s)
 
